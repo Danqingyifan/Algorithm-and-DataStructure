d0586a9ea13aa38fc8625cb637345f40e427879a
Danqingyifan <danqingyifan@gmail.com>
2024-03-22 12:22:45 +0800

    upload

---
+++ Problem/LongestPalindromicSubstring.cpp
@@ -0,0 +1,17 @@
+// Problem Number: 20
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        //Manacher's Algorithm
+
+        
+
+    }
+};
