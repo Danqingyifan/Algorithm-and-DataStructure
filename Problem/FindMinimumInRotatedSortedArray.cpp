2dcfa7aab5dac893287bb9a8e14e8a377878d919
Danqingyifan <danqingyifan@gmail.com>
2024-03-25 21:08:56 +0800

    upload

---
+++ Problem/FindMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,32 @@
+// Problem Number: 35
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <unordered_set>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int low = 0, high = nums.size() - 1;
+
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < nums[high])
+            {
+                high = mid;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return nums[low];
+    }
+};
